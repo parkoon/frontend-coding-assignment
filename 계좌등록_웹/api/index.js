e97fb53f59c398d93dfa20a90e2841c28e397309
parkoon <devparkoon@gmail.com>
2020-04-12 22:24:16 +0900

    add cors

--- 계좌등록_웹/api/index.js
+++ 계좌등록_웹/api/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const http = require("http");
+const cors = require("cors");
+
 const app = express();
 
 const server = http.createServer(app);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 const PORT = process.env.PORT || 3030;
 
